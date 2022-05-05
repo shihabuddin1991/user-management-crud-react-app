9a1cba7ddc5cb395456963f371cb3a29b54f6c2b
anisul-islam <anisul2010s@yahoo.co.uk>
2022-05-05 14:21:28 +0300

    crud user management app

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 const UserForm = ({ handleSubmitData, selectedUser, btnText }) => {
   const [user, setUser] = useState({
